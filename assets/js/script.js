8ffa7bb3c23df90573770242538b283dff927761
tnbKristi <kristi@catalytic.com>
2016-09-27 13:59:56 -0400

    version bump

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(function() {
 
-    if(typeof App !== 'undefined') {
-        App.start({ version: '1.10.2' });
+    if (typeof App !== 'undefined') {
+        App.start({ version: '1.11.0' });
     }
 
 });
