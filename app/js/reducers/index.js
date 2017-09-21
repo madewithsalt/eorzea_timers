e3371ad278f170016a54a7e61ec4b1a3b97c0c94
Kristi Centinaro <kcent@ip-192-168-7-22.ec2.internal>
2017-09-21 17:10:45 -0400

    data cleanup - v2.1.7

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -9,7 +9,7 @@ import page from './pageReducer';
 import watchgroups from './watchGroupsReducer';
 import customlist from './customListReducer';
 
-const version = (state) => "2.1.6";
+const version = (state) => "2.1.7";
 
 const rootReducer = combineReducers({
   clock,
