f4d7ac215787be8e42a326e032134ce0388dce00
Kristi Centinaro <kcent@ip-192-168-7-22.ec2.internal>
2017-09-21 10:35:38 -0400

    update with s3 deploy shortcuts

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -9,7 +9,7 @@ import page from './pageReducer';
 import watchgroups from './watchGroupsReducer';
 import customlist from './customListReducer';
 
-const version = (state) => "2.1.5";
+const version = (state) => "2.1.6";
 
 const rootReducer = combineReducers({
   clock,
