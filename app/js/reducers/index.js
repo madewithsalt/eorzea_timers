a6d0021d33dff3ff8fc6e458d2463ec3c2263da4
Kristi Centinaro <kcent@ip-192-168-7-22.ec2.internal>
2017-10-07 14:35:42 -0400

    2.1.8

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -9,7 +9,7 @@ import page from './pageReducer';
 import watchgroups from './watchGroupsReducer';
 import customlist from './customListReducer';
 
-const version = (state) => "2.1.7";
+const version = (state) => "2.1.8";
 
 const rootReducer = combineReducers({
   clock,
