8c9176363e1d325e9797f6354dfafa5aae759846
Kristi Centinaro <kristi@theresnobox.net>
2018-01-30 21:33:38 -0500

    correct version

--- priv/app.js
+++ priv/app.js
@@ -4484,7 +4484,7 @@ var _customListReducer2 = _interopRequireDefault(_customListReducer);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var version = function version(state) {
-  return "2.1.2";
+  return "2.2.0";
 };
 
 var rootReducer = (0, _redux.combineReducers)({
